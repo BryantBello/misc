83c633ffa52fe45324290acbde416b6c275d0f1b
bryantbello <bryant.bello@outlook.com>
2016-08-04 20:15:27 -0400

    cleaning up repos

--- iview/fuzz.js
+++ iview/fuzz.js
@@ -1,8 +1,13 @@
-alert("testing");
 
-
-var number = [];
-
-
-
-
+var x = 0;
+for (var x=1; x <= 100; x++){
+    if( x % 3 == 0 ) {
+        document.write("fizz");
+    } else {
+        if( x % 5 == 0 ) {
+            document.write("buzz");
+        }
+        if( x % 3 == 0 && x % 5 == 0) {
+            document.write("FizzBuzz");
+        }
+    }
